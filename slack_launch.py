102199e3fa904f138db820934ce5b6e68bb3732f
CodeF53 <37855219+CodeF53@users.noreply.github.com>
2022-08-30 16:46:52 -0600

    dynamic launch location

--- slack_launch.py
+++ slack_launch.py
@@ -1,12 +1,37 @@
+from ensurepip import version
 from electron_inject import inject
 from wget import download
 from os import rename, getcwd, makedirs
+from os import name as osname
+from os.path import expanduser
 from os.path import exists
+import os
 
-#TODO: make script try to automatically determine this
-# windows: "C:/ProgramData/F53/slack/app-4.27.154/slack.exe"
-# linux "/usr/lib/slack/slack"
+# by default have correct install path for linux
 slack_location = "/usr/lib/slack/slack"
+# if system is windows
+if (osname == "nt"):
+    # %appdata% location of slack
+    slack_location = os.getenv('LOCALAPPDATA') + "\\" + "slack"
+    # check if slack is installed
+    if (exists(slack_location)):
+        # get latest slack version
+        # by iterating through all subdirectorys
+        # and getting the one with the newest slack.exe
+        latestCreatedTime = 0
+        biggestVersionName = ""
+        for subdir in next(os.walk(slack_location))[1]:
+            # filter out non "app-version" dirs
+            if ("app-" in subdir):
+                createdTime = os.stat(slack_location+"\\slack.exe").st_ctime
+                if (createdTime>latestCreatedTime):
+                    latestCreatedTime = createdTime
+                    biggestVersionName = subdir
+        slack_location = slack_location + "\\" + biggestVersionName + "\\slack.exe"
+    else:
+        # yell at user
+        print("Could not find your slack installation")
+print(slack_location)
 
 # scripts we will inject are added to this array
 scripts = []
@@ -14,7 +39,7 @@ scripts = []
 # or the Current Working Directory
 cwd = getcwd()
 # make the libs folder if it doesn't exist
-libsFolder = f"{cwd}/libs"
+libsFolder = f"{cwd}\\libs"
 if not exists(libsFolder):
     makedirs(libsFolder)
 # array of libraries to install
@@ -28,7 +53,7 @@ for libURL in libURLs:
     print(f"\nGetting library file: {fileName}")
 
     # check if we have already downloaded it
-    if exists(f"libs/{fileName}"):
+    if exists(f"libs\\{fileName}"):
         print(f"\tfound {fileName} in libs")
     else:
         print(f"\t{fileName} not in libs, downloading")
@@ -39,14 +64,14 @@ for libURL in libURLs:
     # add path to file to list of scripts to inject
     # we use realpath() here to get the fill directory to the file
     # because sometimes electron inject gets screwy with relative files
-    scripts.append(f"{libsFolder}/{fileName}")
+    scripts.append(f"{libsFolder}\\{fileName}")
 # add our own code as the last entry in scripts
 # so all the scripts are loaded once our code runs
-scripts.append(f"{cwd}/inject.js")
+scripts.append(f"{cwd}\\inject.js")
 
 print(f"\ncalling electron injector with args:")
 print(f"\tslack location:{slack_location}")
 print(f"\tscripts:")
 for script in scripts: print(f"\t\t{script}")
 
-inject(slack_location, devtools=False, timeout=600, scripts=scripts) 
+inject(slack_location, devtools=False, timeout=60000, scripts=scripts) 
